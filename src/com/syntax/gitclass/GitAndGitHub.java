76596fb23a8c4e51eb178ece11863e0da62111b4
Elisa <ealia3112@gmail.com>
2022-05-31 19:54:45 -0400

    2nd commit

--- src/com/syntax/gitclass/GitAndGitHub.java
+++ src/com/syntax/gitclass/GitAndGitHub.java
@@ -1,5 +1,10 @@
 package com.syntax.gitclass;
 
 public class GitAndGitHub {
-
+ 
+	public static void main(String [] args) {
+		
+		System.out.print("today we learn github");
+		System.out.print("today is a very good day");
+	}
 }
